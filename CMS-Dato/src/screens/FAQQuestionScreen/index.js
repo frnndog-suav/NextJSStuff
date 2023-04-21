b2285a8d6709706867b830dbee19aeb125698c3b
Fernando Goia <f97.goya@gmail.com>
2023-04-21 15:32:06 -0300

    Global content

--- CMS-Dato/src/screens/FAQQuestionScreen/index.js
+++ CMS-Dato/src/screens/FAQQuestionScreen/index.js
@@ -34,13 +34,12 @@ export async function getStaticProps({ params }) {
   return {
     props: {
       id,
-      title: data.contentFaqQuestion.title,
-      content: data.contentFaqQuestion.content,
+      cmsContent: data,
     },
   };
 }
 
-export default function FAQQuestionScreen({ title, content }) {
+export default function FAQQuestionScreen({ cmsContent }) {
   return (
     <>
       <Head>
@@ -67,14 +66,14 @@ export default function FAQQuestionScreen({ title, content }) {
           }}
         >
           <Text tag="h1" variant="heading1">
-            {title}
+            {cmsContent.contentFaqQuestion.title}
           </Text>
 
           {/* <Box dangerouslySetInnerHTML={{ __html: content }} /> */}
           {/* <pre>{JSON.stringify(content, null, 4)}</pre> */}
 
           <StructuredText
-            data={content}
+            data={cmsContent.contentFaqQuestion.content}
             customNodeRules={[
               renderNodeRule(isHeading, ({ node, children, key }) => {
                 const tag = `h${node.level}`;
@@ -91,7 +90,7 @@ export default function FAQQuestionScreen({ title, content }) {
         </Box>
       </Box>
 
-      <Footer />
+      <Footer description={cmsContent.globalContent.globalFooter.description} />
     </>
   );
 }
--- CMS-Dato/src/service/cms/cmsService.js
+++ CMS-Dato/src/service/cms/cmsService.js
@@ -1,5 +1,13 @@
 const DATO_TOKEN = process.env.DATO_TOKEN;
 
+const globalQuery = `
+query {
+  globalFooter{
+    description
+  }
+}
+`;
+
 export async function cmsService({ query }) {
   try {
     const pageContentResponse = await fetch("https://graphql.datocms.com/", {
@@ -17,8 +25,28 @@ export async function cmsService({ query }) {
       throw new Error(JSON.stringify(body));
     });
 
+    const globalContentResponse = await fetch("https://graphql.datocms.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${DATO_TOKEN}`,
+      },
+      body: JSON.stringify({
+        query: globalQuery,
+      }),
+    }).then(async (response) => {
+      const body = await response.json();
+      if (!body.errors) return body;
+      throw new Error(JSON.stringify(body));
+    });
+
     return {
-      data: pageContentResponse.data,
+      data: {
+        ...pageContentResponse.data,
+        globalContent: {
+          ...globalContentResponse.data,
+        },
+      },
     };
   } catch (err) {
     throw new Error(err.message);
