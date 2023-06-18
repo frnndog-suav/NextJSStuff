6bb612f495b144c9e1bed1594aa9eebc21d4f036
Fernando Goia <f97.goya@gmail.com>
2023-06-18 19:23:55 -0300

    Moving react router dom tutorial to ReactStuff repo

--- react-router-dom-tutorial/src/components/NavBar.tsx
+++
@@ -1,8 +0,0 @@
-import { Link } from "react-router-dom";
-
-export const NavBar = () => (
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/contact">Contato</Link>
-  </nav>
-);
--- react-router-dom-tutorial/src/routes/ErrorPage.tsx
+++
@@ -1,5 +0,0 @@
-export const ErrorPage = () => (
-  <div>
-    <h1>Error 404</h1>
-  </div>
-);
