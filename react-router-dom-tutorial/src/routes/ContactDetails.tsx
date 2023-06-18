8e4c022c602250bc457364ba771560f50dbb5996
Fernando Goia <f97.goya@gmail.com>
2023-06-18 19:03:19 -0300

    useNavigate (redirect)

--- react-router-dom-tutorial/src/routes/ContactDetails.tsx
+++ react-router-dom-tutorial/src/routes/ContactDetails.tsx
@@ -1,11 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const ContactDetails = () => {
   const { nomeDaVariavel } = useParams();
 
+  // 6 - redirect
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate("/");
+  };
+
   return (
     <div>
       <h1>Exibindo dado da url: {nomeDaVariavel}</h1>
+      <button onClick={handleRedirect}>Voltar para home</button>
     </div>
   );
 };
