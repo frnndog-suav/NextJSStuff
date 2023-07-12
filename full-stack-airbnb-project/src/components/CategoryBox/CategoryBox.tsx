dfd812e3197c8a88de46bbe5cccd143c11e51c02
Fernando Goia <f97.goya@gmail.com>
2023-07-11 22:04:05 -0300

    Creating custom url based on selected category

--- full-stack-airbnb-project/src/components/CategoryBox/CategoryBox.tsx
+++ full-stack-airbnb-project/src/components/CategoryBox/CategoryBox.tsx
@@ -1,4 +1,6 @@
-import { FC } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
+import { FC, useCallback } from "react";
 import { IconType } from "react-icons";
 
 interface CategoryBoxProps {
@@ -8,8 +10,38 @@ interface CategoryBoxProps {
 }
 
 const CategoryBox: FC<CategoryBoxProps> = ({ icon: Icon, label, selected }) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, params, router]);
+
   return (
     <div
+      onClick={handleClick}
       className={`
       flex 
       flex-col
