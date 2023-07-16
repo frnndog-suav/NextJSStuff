b9d0a327bf5aefc15ed10e8b6fc3dda59372d91f
Fernando Goia <f97.goya@gmail.com>
2023-07-16 17:03:23 -0300

    Configuring rent modal options to form

--- full-stack-airbnb-project/src/components/Modals/RentModal/RentModal.tsx
+++ full-stack-airbnb-project/src/components/Modals/RentModal/RentModal.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import CategoryInput from "@/components/Input/CategoryInput/CategoryInput";
+import { categories } from "@/components/Navbar/Categories/Categories";
 import useRentModal from "@/hooks/useRentModal/useRentModal";
 import { useMemo, useState } from "react";
-import Modal from "../Modal/Modal";
+import { FieldValues, useForm } from "react-hook-form";
 import Heading from "../Heading/Heading";
-import { categories } from "@/components/Navbar/Categories/Categories";
-import CategoryInput from "@/components/Input/CategoryInput/CategoryInput";
+import Modal from "../Modal/Modal";
 
 export enum Step {
   Category = 0,
@@ -20,6 +21,36 @@ const RentModal = () => {
   const rentModal = useRentModal();
 
   const [step, setStep] = useState<Step>(Step.Category);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: "",
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      description: "",
+    },
+  });
+
+  const category = watch("category");
+
+  const setCustomValue = (id: string, value: any) =>
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
 
   const onBack = () => {
     setStep((value) => value - 1);
@@ -52,13 +83,13 @@ const RentModal = () => {
         subtitle="Pick a category"
       />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
-        {categories.map((category) => (
-          <div key={category.id} className="col-span-1">
-            <CategoryInput 
-              onClick={() => {}}
-              selected={false}
-              label={category.label}
-              icon={category.icon}
+        {categories.map((item) => (
+          <div key={item.id} className="col-span-1">
+            <CategoryInput
+              onClick={(category) => setCustomValue("category", category)}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
             />
           </div>
         ))}
