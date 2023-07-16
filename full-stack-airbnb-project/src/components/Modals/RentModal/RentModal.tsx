a775ef081b52e335b820ae185749e8dce6fd2216
Fernando Goia <f97.goya@gmail.com>
2023-07-16 18:12:31 -0300

    Implementing step 3 of rental modal

--- full-stack-airbnb-project/src/components/Modals/RentModal/RentModal.tsx
+++ full-stack-airbnb-project/src/components/Modals/RentModal/RentModal.tsx
@@ -9,6 +9,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import Heading from "../Heading/Heading";
 import Modal from "../Modal/Modal";
 import dynamic from "next/dynamic";
+import Counter from "@/components/Input/Counter/Counter";
 
 export enum Step {
   Category = 0,
@@ -47,6 +48,9 @@ const RentModal = () => {
 
   const category = watch("category");
   const location = watch("location");
+  const guestCount = watch("guestCount");
+  const roomCount = watch("roomCount");
+  const bathroomCount = watch("bathroomCount");
 
   const Map = useMemo(
     () =>
@@ -120,6 +124,37 @@ const RentModal = () => {
           value={location}
         />
         <Map center={location?.latlng} />
+      </div>
+    );
+  }
+
+  if (step === Step.Info) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basic about your place"
+          subtitle="What amenities do you have?"
+        />
+        <Counter
+          title="Guests"
+          subtitle="How many guests do you allow?"
+          value={guestCount}
+          onChange={(value) => setCustomValue("guestCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+          value={roomCount}
+          onChange={(value) => setCustomValue("roomCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+          value={bathroomCount}
+          onChange={(value) => setCustomValue("bathroomCount", value)}
+        />
       </div>
     );
   }
