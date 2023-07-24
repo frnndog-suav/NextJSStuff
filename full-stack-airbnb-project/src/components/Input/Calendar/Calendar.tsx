903d93c57ed4d0b5caf178706576dc8a92d894f0
Fernando Goia <f97.goya@gmail.com>
2023-07-24 20:34:19 -0300

    Creating Calendar component

--- full-stack-airbnb-project/src/components/Input/Calendar/Calendar.tsx
+++ full-stack-airbnb-project/src/components/Input/Calendar/Calendar.tsx
@@ -3,8 +3,8 @@
 import { FC } from "react";
 import { DateRange, Range, RangeKeyDict } from "react-date-range";
 
-import 'react-date-range/dist/styles'
-import 'react-date-range/dist/theme/default.css'
+import "react-date-range/dist/styles";
+import "react-date-range/dist/theme/default.css";
 
 interface CalendarProps {
   value: Range;
@@ -13,10 +13,18 @@ interface CalendarProps {
 }
 
 const Calendar: FC<CalendarProps> = ({ onChange, value, disabledDates }) => {
-  return <DateRange
-  
-    
-  />
+  return (
+    <DateRange
+      rangeColors={["#262626"]}
+      ranges={[value]}
+      date={new Date()}
+      onChange={onChange}
+      direction="vertical"
+      showDateDisplay={false}
+      minDate={new Date()}
+      disabledDates={disabledDates}
+    />
+  );
 };
 
 export default Calendar;
