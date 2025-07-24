bea25a9580be8c72066a60c59868dd91642d3b78
Fernando <frnnd.goia97@gmail.com>
2025-07-24 17:58:01 -0300

    Downgrading tailwindcss version

---
+++ site-blog/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen min-w-full bg-slate-400">
+      <h2 className="text-4xl text-green-400">Hello World</h2>
+    </div>
+  );
+}
