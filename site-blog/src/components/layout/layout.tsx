933b58b2668d8da27b092228e1540de771accf1a
fernando.goia <fernando.goia@acqio.com.br>
2025-11-05 09:09:33 -0300

    refactor: adjusting font usage in components

--- site-blog/src/components/layout/layout.tsx
+++ site-blog/src/components/layout/layout.tsx
@@ -1,4 +1,4 @@
-import { Inter } from "next/font/google";
+import { Inter, PT_Sans_Caption } from "next/font/google";
 import { Footer } from "../footer";
 import { Header } from "../header";
 
@@ -6,15 +6,24 @@ type TLayoutProps = {
   children: React.ReactNode;
 };
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  variable: "--font-inter", // mesmo nome contindo no arquivo de configuração tailwind.config.ts
+});
+const ptSansCaption = PT_Sans_Caption({
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-sans", // mesmo nome contindo no arquivo de configuração tailwind.config.ts
+});
 
 export function Layout({ children }: TLayoutProps) {
   return (
     <div
-      className={`${inter.className} relative flex min-h-screen flex-col dark`}
+      className={`${inter.className} ${ptSansCaption.className} relative flex min-h-screen flex-col dark font-inter`}
     >
       <Header />
-      <main className="flex-1 flex flex-col mb-12">{children}</main>
+      <main className="flex-1 flex flex-col mb-12 mt-10">{children}</main>
       <Footer />
     </div>
   );
