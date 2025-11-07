6b3f29ae35156526f7a90e6e8829021201a780d1
fernando.goia <fernando.goia@acqio.com.br>
2025-11-07 08:56:14 -0300

    feat: implemeting not found page

--- site-blog-app-router/src/components/search/search.tsx
+++ site-blog-app-router/src/components/search/search.tsx
@@ -1,12 +1,14 @@
 import { cn } from "@/lib/utils";
 import { CircleX, SearchIcon } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 export function Search() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const router = useRouter();
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
   const query = searchParams?.get("q") || "";
+  const hasQuery = !!searchParams?.has("q");
 
   const handleSearch = useCallback(
     (event: React.FormEvent) => {
@@ -22,16 +24,22 @@ export function Search() {
   function handleQueryChange(event: React.ChangeEvent<HTMLInputElement>) {
     const newQuery = event.target.value;
 
-    router.push(`/blog?q=${encodeURIComponent(newQuery)}`, {      
+    router.push(`/blog?q=${encodeURIComponent(newQuery)}`, {
       scroll: false,
     });
   }
 
   function resetSearch() {
-    router.push("/blog", {      
+    router.push("/blog", {
       scroll: false,
     });
   }
+
+  useEffect(() => {
+    if (hasQuery) {
+      inputRef.current?.focus();
+    }
+  }, [hasQuery]);
 
   return (
     <form onSubmit={handleSearch} className="relative group w-full md:w-60">
@@ -42,6 +50,7 @@ export function Search() {
         )}
       />
       <input
+        ref={inputRef}
         type="text"
         placeholder="Buscar"
         value={query}
