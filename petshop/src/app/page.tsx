a3f1dfe0ea7f1df31a2220a41ff0220215dc92ad
fernando.goia <fernando.goia@acqio.com.br>
2025-11-11 10:06:24 -0300

    chore (page): basic structure of home page

--- petshop/src/app/page.tsx
+++ petshop/src/app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <div>
-      <h2 className="text-red-400">Hello World</h2>
+    <div className="bg-background-primary p-6">
+      <div className="flex items-center justify-between md:m-8">
+        <div>
+          <h1 className="text-title-size text-content-primary mb-2">
+            Sua Agenda
+          </h1>
+          <p className="text-paragraph-small-size text-content-secondary">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
