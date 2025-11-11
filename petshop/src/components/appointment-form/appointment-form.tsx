3e289519f75fd6030eb7732b15dd978ca7fa7f1d
fernando.goia <fernando.goia@acqio.com.br>
2025-11-11 12:57:49 -0300

    feat (form): implementing basic structure of form

--- petshop/src/components/appointment-form/appointment-form.tsx
+++ petshop/src/components/appointment-form/appointment-form.tsx
@@ -1,29 +1,64 @@
 'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
 import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog';
+import { useForm } from 'react-hook-form';
+import z from 'zod';
 import { Button } from '../ui/button';
 import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
 } from '../ui/dialog';
+import { Form } from '../ui/form';
+
+const appointmentFormSchema = z.object({
+  tutorName: z.string().min(3, 'O nome do tutor é obrigatório'),
+  petName: z.string().min(3, 'O nome do pet é obrigatório'),
+  phone: z.string().min(11, 'O telefone é obrigatório'),
+  description: z.string().optional(),
+});
+
+type TAppointmentFormValues = z.infer<typeof appointmentFormSchema>;
 
 export function AppointmentForm() {
+  const form = useForm<TAppointmentFormValues>({
+    resolver: zodResolver(appointmentFormSchema),
+    defaultValues: {
+      phone: '',
+      petName: '',
+      tutorName: '',
+      description: '',
+    },
+  });
+
+  function onSubmit(data: TAppointmentFormValues) {
+    console.log(data);
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="brand">Novo agendamento</Button>
       </DialogTrigger>
 
-      <DialogContent>
+      <DialogContent
+        variant="appointment"
+        overlayVariant="blurred"
+        showCloseButton={true}
+      >
         <DialogHeader>
           <DialogTitle>Agende um atendimento</DialogTitle>
+
+          <DialogDescription>
+            Digite os dados do cliente para realizar o agendamento.
+          </DialogDescription>
         </DialogHeader>
 
-        <DialogDescription>
-          Digite os dados do cliente para realizar o agendamento.
-        </DialogDescription>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}></form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
