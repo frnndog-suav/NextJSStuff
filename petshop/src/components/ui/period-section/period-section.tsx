2d0cb9542d65194f4372de87716d07cc980ee3fe
fernando.goia <fernando.goia@acqio.com.br>
2025-11-11 11:05:58 -0300

    feat: implementing period section component

--- petshop/src/components/ui/period-section/period-section.tsx
+++ petshop/src/components/ui/period-section/period-section.tsx
@@ -27,6 +27,25 @@ export function PeriodSection({ period }: TPeriodSectionsProps) {
           {period.timeRange}
         </span>
       </div>
+
+      {period.appointments.length > 0 ? (
+        <div className="px-5">
+          <div>
+            <div className="grid grid-cols-2 md:hidden text-label-small-size text-content-secondary mb-2">
+              <div className="text-left">Horário</div>
+              <div className="text-right">Paciente</div>
+            </div>
+
+            {period.appointments.map((appointment, index) => (
+              <div key={index}>{appointment.petName}</div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p>Nenhum agendamento para este período</p>
+        </div>
+      )}
     </section>
   );
 }
